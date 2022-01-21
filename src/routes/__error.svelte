daff76db42126d8d9aa1a05844b0c6c5929f8a89
lochnessdragon <lochnessdragon@github.com>
2022-01-21 02:09:34 -0600

    Added error handling

---
+++ src/routes/__error.svelte
@@ -0,0 +1,51 @@
+<script context="module">
+	/** @type {import('@sveltejs/kit').ErrorLoad} */
+	export function load({ error, status }) {
+        const comments = [
+            "I think you've explored a bit too far...",
+            "Houston.... We have a problem!",
+            "We're very sorry for the inconvenice. Some intern is going to be fired.",
+            "🐒",
+            "We rolled a nat 1...",
+            "Alex, did you let Jerry into the server room again?",
+            "Website 100",
+            "Red pill or Blue pill?",
+            "You sunk my Battleship!",
+            "Looks like a creeper got into the server room again...",
+            "$%#$%&#$%#%$#%#$#%$# technology",
+            "This is why you NEVER mess with Tiamat.",
+            "Knock knock. Who's there? Error 404: Joke Not Found",
+            "Indiana Jones and the search for the missing web page.",
+            "At least this is better than Jar Jar..."
+        ];
+
+        let comment = comments[Math.floor(Math.random() * comments.length)];
+
+		return {
+			props: {
+                error_code: `${status}`,
+                error_message: `${error.message}`,
+                comment: `${comment}`
+			}
+		};
+	}
+</script>
+
+<script>
+    export let error_code;
+    export let error_message;
+    export let comment;
+</script>
+
+<svelte:head>
+    <title>{error_code}: {error_message}</title>
+</svelte:head>
+
+<div class="text-center py-40">
+    <h1 class="text-9xl font-black text-blue-600">{error_code}</h1>
+    <p class="text-red-600">{error_message}</p><br/>
+    <p class="text-red-600">{comment}</p><br/>
+    <!-- Sarcastic Comment -->
+
+    <a href="/" class="bg-blue-600 text-white rounded-3xl p-2.5 text-lg hover:p-3.5 hover:text-xl hover:bg-orange-600 transition-all"><button>Go home →</button></a>
+</div>
