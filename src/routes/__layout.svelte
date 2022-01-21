3bd08833a5b09db3ee542451a97c1bf0fd9751da
lochnessdragon <lochnessdragon@github.com>
2022-01-21 01:36:22 -0600

    Added basic site layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,3 +3,11 @@
 </script>
 
 <slot />
+
+<!-- footer -->
+<div class="bg-slate-700 text-slate-100 py-9">
+	<p class="text-center">
+	© 2022 Copyright: <span class="font-medium">lochnessdragon</span>
+	</p>
+</div>
+<!-- /footer -->
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,4 +2,4 @@
     <title>Unnamed Game Project</title>
 </svelte:head>
 
-<h1>Welcome to my Untitled Game Project's Website!</h1>
+<h1 class="text-lg">Welcome to my Untitled Game Project's Website!</h1>
